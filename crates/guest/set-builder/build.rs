f13424b6cbfff02f94bd356899dfed425959bcf1
Victor Graf <victor@risczero.com>
2024-11-25 09:40:50 -0800

    BM-356: Fix and improve guest build.rs scripts (#167)
    Based on feedback from @Walloc on
    https://github.com/risc0/risc0-ethereum/pull/336, this PR includes
    improvements and fixes to guest `build.rs` scripts. At least for me,
    this also fixes `cargo clippy`, which has not need working in my editor.

--- crates/guest/set-builder/build.rs
+++ crates/guest/set-builder/build.rs
@@ -2,7 +2,7 @@
 //
 // All rights reserved.
 
-use std::{collections::HashMap, env};
+use std::{collections::HashMap, env, path::PathBuf};
 
 use risc0_build::{embed_methods_with_options, DockerOptions, GuestOptions};
 use risc0_build_ethereum::generate_solidity_files;
@@ -12,16 +12,14 @@ const SOLIDITY_IMAGE_ID_PATH: &str = "../../../contracts/src/SetBuilderImageID.s
 const SOLIDITY_ELF_PATH: &str = "../../../contracts/test/SetBuilderElf.sol";
 
 fn main() {
-    if env::var("RISC0_SKIP_BUILD").is_ok() {
-        return;
-    }
-
     // Builds can be made deterministic, and thereby reproducible, by using Docker to build the
     // guest. Check the RISC0_USE_DOCKER variable and use Docker to build the guest if set.
     println!("cargo:rerun-if-env-changed=RISC0_USE_DOCKER");
+    println!("cargo:rerun-if-changed=build.rs");
+    let manifest_dir = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
     let use_docker = env::var("RISC0_USE_DOCKER")
         .ok()
-        .map(|_| DockerOptions { root_dir: Some("../../../".into()) });
+        .map(|_| DockerOptions { root_dir: Some(manifest_dir.join("../../..")) });
 
     // Generate Rust source files for the methods crate.
     let guests = embed_methods_with_options(HashMap::from([(
