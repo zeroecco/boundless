e4891d4d8e996219badd493f010412dfdb96b98a
hexcow <way-scarp4u@icloud.com>
2025-03-07 01:24:32 +0300

    fix: Fixed missing argument check for JOB variable in the script (#356)
    I’ve added a check for the JOB variable to ensure it’s passed as an
    argument when the script is run. If it’s missing, a helpful error
    message will be displayed instead of the script crashing.
    
    This improves error handling and makes the script more user-friendly.
    
    Co-authored-by: Victor Snyder-Graf <victor@risczero.com>

--- scripts/job_status.sh
+++ scripts/job_status.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 source .env-compose
+
 JOB=$1
+if [ -z "$JOB" ]; then
+    echo "Error: No job ID provided."
+    exit 1
+fi
 
 PGPASSWORD="${POSTGRES_PASSWORD}" psql -h 127.0.0.1 -U "${POSTGRES_USER}" "${POSTGRES_DB}" -c "SELECT count(task_id) as jobs_count FROM tasks WHERE job_id = '${JOB}';"
 
@@ -11,7 +16,6 @@ PGPASSWORD="${POSTGRES_PASSWORD}" psql -h 127.0.0.1 -U "${POSTGRES_USER}" "${POS
 
 echo 'task times (totals):'
 PGPASSWORD="${POSTGRES_PASSWORD}" psql -h 127.0.0.1 -U "${POSTGRES_USER}" "${POSTGRES_DB}" -c "SELECT jsonb_object_keys(task_def) as task_type, COUNT(*) as completed_count, SUM(EXTRACT(EPOCH FROM updated_at) - EXTRACT(EPOCH FROM started_at)) as total_secs FROM tasks WHERE job_id = '${JOB}' AND state = 'done' AND started_at IS NOT NULL AND updated_at IS NOT NULL GROUP BY jsonb_object_keys(task_def) ORDER BY total_secs DESC;"
-
 
 echo "Effective Hz:"
 PGPASSWORD="${POSTGRES_PASSWORD}" psql -h 127.0.0.1 -U "${POSTGRES_USER}" "${POSTGRES_DB}" -c "SELECT CAST(res1.total_cycles AS decimal)  / res2.elapsed_sec AS hz, res1.total_cycles, res2.elapsed_sec FROM (SELECT output->'total_cycles' total_cycles FROM tasks WHERE task_id = 'init' AND job_id = '${JOB}') res1, (SELECT EXTRACT(EPOCH FROM (MAX(updated_at) - MIN(started_at))) AS elapsed_sec FROM tasks WHERE job_id = '${JOB}') res2;"
