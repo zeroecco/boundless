dac7106b5a78b0ec568f12a1dd48f77cc18c39bd
Willem Olding <willemolding@gmail.com>
2025-03-13 10:57:10 +1300

    BM-621: Add configurable warnings and error logs for stake balance (#391)
    Closes #390 
    
    - Adds new public method to `boundless_market` -
    `with_stake_balance_alert` which takes a warning and error threshold to
    alert on
    - Adds new config fields to broker to set these on the market in
    `order_monitor`
    - Adds to existing test to check that market is correctly alerting when
    stake balance goes below thresholds

--- crates/broker/src/config.rs
+++ crates/broker/src/config.rs
@@ -93,6 +93,12 @@ pub struct MarketConf {
     /// Gas estimate for fulfill call to use if it cannot be estimated using the node RPC
     #[serde(default = "defaults::fulfill_gas_estimate")]
     pub fulfill_gas_estimate: u64,
+    /// Stake balance warning threshold (in stake tokens)
+    /// if the stake balance drops below this the broker will issue warning logs
+    pub stake_balance_warn_threshold: Option<String>,
+    /// Stake balance error threshold (in stake tokens)
+    /// if the stake balance drops below this the broker will issue error logs
+    pub stake_balance_error_threshold: Option<String>,
 }
 
 impl Default for MarketConf {
@@ -113,6 +119,8 @@ impl Default for MarketConf {
             max_fetch_retries: Some(2),
             lockin_gas_estimate: defaults::lockin_gas_estimate(),
             fulfill_gas_estimate: defaults::fulfill_gas_estimate(),
+            stake_balance_warn_threshold: None,
+            stake_balance_error_threshold: None,
         }
     }
 }
