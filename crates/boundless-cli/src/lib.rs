0e20a556865ae8f9f8b46e712e202dec167cd661
Angelo Capossele <angelocapossele@gmail.com>
2025-03-20 15:04:32 +0000

    BM-665: Fix running tests on CI (#423)
    CI wasn't running any rust test on CI due to a wrong `ignored` keyword

--- crates/boundless-cli/src/lib.rs
+++ crates/boundless-cli/src/lib.rs
@@ -312,7 +312,7 @@ mod tests {
                 predicate: Predicate::prefix_match(vec![1]),
             },
             format!("file://{ECHO_PATH}"),
-            Input::inline(vec![1, 2, 3, 4]),
+            Input::builder().write_slice(&[1, 2, 3, 4]).build_inline().unwrap(),
             Offer::default(),
         );
 
