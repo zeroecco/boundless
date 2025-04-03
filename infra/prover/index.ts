916919c5cc11de346d4e50d3adac6549c59978d0
Austin Abell <austinabell8@gmail.com>
2025-04-03 13:46:59 -0400

    wip switching from EFS/fargate to ec2

--- infra/prover/index.ts
+++ infra/prover/index.ts
@@ -12,7 +12,7 @@ export = () => {
   const isDev = pulumi.getStack() === "dev";
   const prefix = isDev ? `${getEnvVar("DEV_NAME")}-` : "";
   const serviceName = `${prefix}bonsai-prover`;
-  
+
   const baseStackName = config.require('BASE_STACK');
   const baseStack = new pulumi.StackReference(baseStackName);
   const vpcId = baseStack.getOutput('VPC_ID');
@@ -64,24 +64,6 @@ export = () => {
     source: fileToUpload,
   });
 
-  // EFS
-  const fileSystem = new aws.efs.FileSystem(serviceName, {
-    encrypted: true,
-    tags: {
-      Name: serviceName,
-    },
-  });
-
-  privSubNetIds.apply((subnets) =>
-    subnets.map((subnetId: string, index: number) => {
-      return new aws.efs.MountTarget(`${serviceName}-mount-${index}`, {
-        fileSystemId: fileSystem.id,
-        subnetId: subnetId,
-        securityGroups: [brokerSecGroup.id],
-      });
-    })
-  );
-
   const taskRole = new aws.iam.Role(serviceName, {
     assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
       Service: 'ecs-tasks.amazonaws.com',
@@ -101,7 +83,39 @@ export = () => {
         {
           Effect: 'Allow',
           Action: ['secretsmanager:GetSecretValue', 'ssm:GetParameters'],
-          Resource: [privateKeySecret.arn, bonsaiSecret.arn, ethRpcUrlSecret.arn],
+          Resource: [privateKeySecret.arn, bonsaiSecret.arn],
+        },
+      ],
+    },
+  });
+
+  const execRole = new aws.iam.Role(`exec-${serviceName}`, {
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+      Service: 'ecs-tasks.amazonaws.com',
+    }),
+  });
+
+  const _execRolePolicy = new aws.iam.RolePolicy(`exec-${serviceName}`, {
+    role: execRole.id,
+    policy: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Action: [
+            'ecr:GetAuthorizationToken',
+            'ecr:BatchCheckLayerAvailability',
+            'ecr:GetDownloadUrlForLayer',
+            'ecr:BatchGetImage',
+            'logs:CreateLogStream',
+            'logs:PutLogEvents',
+          ],
+          Resource: '*',
+        },
+        {
+          Effect: 'Allow',
+          Action: ['secretsmanager:GetSecretValue', 'ssm:GetParameters'],
+          Resource: [privateKeySecret.arn, bonsaiSecret.arn],
         },
       ],
     },
@@ -121,54 +135,14 @@ export = () => {
     name: serviceName,
   });
 
-  const executionRole = new aws.iam.Role(`${serviceName}-ecs-execution-role`, {
-    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
-      Service: 'ecs-tasks.amazonaws.com',
-    }),
-  });
-
-  brokerEcr.repository.arn.apply(arn => {
-    new aws.iam.RolePolicy(`${serviceName}-ecs-execution-pol`, {
-      role: executionRole.id,
-      policy: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Effect: 'Allow',
-            Action: [
-              'ecr:GetAuthorizationToken',
-              'ecr:BatchCheckLayerAvailability',
-              'ecr:GetDownloadUrlForLayer',
-              'ecr:BatchGetImage',
-            ],
-            Resource: '*',
-          },
-          {
-            Effect: 'Allow',
-            Action: [
-              'logs:CreateLogStream',
-              'logs:PutLogEvents',
-            ],
-            Resource: '*',
-          },
-          {
-            Effect: 'Allow',
-            Action: ['secretsmanager:GetSecretValue', 'ssm:GetParameters'],
-            Resource: [privateKeySecret.arn, bonsaiSecret.arn, ethRpcUrlSecret.arn],
-          },
-        ],
-      },
-    });
-  })
-
   const authToken = aws.ecr.getAuthorizationTokenOutput({
     registryId: brokerEcr.repository.registryId,
   });
 
-  // Optionally add in the gh token secret and sccache s3 creds to the build ctx
+  // Optionally add in the sccache s3 creds to the build ctx
   let buildSecrets = {};
   if (ciCacheSecret !== undefined) {
-    const cacheFileData = ciCacheSecret.apply((filePath) => fs.readFileSync(filePath, 'utf8'));
+    const cacheFileData = ciCacheSecret.apply((filePath: any) => fs.readFileSync(filePath, 'utf8'));
     buildSecrets = {
       ci_cache_creds: cacheFileData,
     };
@@ -182,49 +156,45 @@ export = () => {
 
   const dockerTagPath = pulumi.interpolate`${brokerEcr.repository.repositoryUrl}:${dockerTag}`;
 
-  const image = new docker_build.Image(serviceName, {
-    tags: [dockerTagPath],
-    context: {
-      location: dockerDir,
-    },
-    platforms: ['linux/amd64'],
-    push: true,
-    dockerfile: {
-      location: `${dockerDir}/dockerfiles/broker.dockerfile`,
-    },
-    buildArgs: {
-      S3_CACHE_PREFIX: 'private/boundless/rust-cache-docker-Linux-X64/sccache',
-    },
-    secrets: buildSecrets,
-    cacheFrom: [
-      {
-        registry: {
-          ref: pulumi.interpolate`${brokerEcr.repository.repositoryUrl}:cache`,
-        },
-      },
-    ],
-    cacheTo: [
-      {
-        registry: {
-          mode: docker_build.CacheMode.Max,
-          imageManifest: true,
-          ociMediaTypes: true,
-          ref: pulumi.interpolate`${brokerEcr.repository.repositoryUrl}:cache`,
-        },
-      },
-    ],
-    registries: [
-      {
-        address: brokerEcr.repository.repositoryUrl,
-        password: authToken.password,
-        username: authToken.userName,
-      },
-    ],
-  });
-
-  const cluster = new aws.ecs.Cluster(serviceName, {
-    name: serviceName,
-  });
+  // TODO use this to build? Can this be used in ec2?
+  // const image = new docker_build.Image(serviceName, {
+  //   tags: [dockerTagPath],
+  //   context: {
+  //     location: dockerDir,
+  //   },
+  //   platforms: ['linux/amd64'],
+  //   push: true,
+  //   dockerfile: {
+  //     location: `${dockerDir}/dockerfiles/broker.dockerfile`,
+  //   },
+  //   buildArgs: {
+  //     S3_CACHE_PREFIX: 'private/boundless/rust-cache-docker-Linux-X64/sccache',
+  //   },
+  //   secrets: buildSecrets,
+  //   cacheFrom: [
+  //     {
+  //       registry: {
+  //         ref: pulumi.interpolate`${brokerEcr.repository.repositoryUrl}:cache`,
+  //       },
+  //     },
+  //   ],
+  //   cacheTo: [
+  //     {
+  //       registry: {
+  //         imageManifest: true,
+  //         ociMediaTypes: true,
+  //         ref: pulumi.interpolate`${brokerEcr.repository.repositoryUrl}:cache`,
+  //       },
+  //     },
+  //   ],
+  //   registries: [
+  //     {
+  //       address: brokerEcr.repository.repositoryUrl,
+  //       password: authToken.password,
+  //       username: authToken.userName,
+  //     },
+  //   ],
+  // });
 
   const brokerSecGroup = new aws.ec2.SecurityGroup(serviceName, {
     name: serviceName,
@@ -240,102 +210,106 @@ export = () => {
     ],
   });
 
-  const _efsInbound = new aws.ec2.SecurityGroupRule(`${serviceName}-efs-inbound`, {
-    type: 'ingress',
-    fromPort: 2049,
-    toPort: 2049,
-    protocol: 'tcp',
-    securityGroupId: brokerSecGroup.id,
-    sourceSecurityGroupId: brokerSecGroup.id,
+  // Set up CloudWatch logging for the EC2 instance
+  const brokerLogGroup = new aws.cloudwatch.LogGroup(serviceName, {
+    name: serviceName,
+    retentionInDays: 0,
+    skipDestroy: true,
   });
 
-  const brokerS3BucketName = brokerS3Bucket.bucket.apply(n => n);
 
-  const service = new awsx.ecs.FargateService(serviceName, {
-    name: serviceName,
-    cluster: cluster.arn,
-    networkConfiguration: {
-      securityGroups: [brokerSecGroup.id],
-      assignPublicIp: false,
-      subnets: privSubNetIds,
+  // Create an EC2 instance
+  const ec2Instance = new aws.ec2.Instance(serviceName, {
+    ami: "ami-087f352c165340ea1", // Free tier x86_64
+    instanceType: "t2.micro", // Free tier instance type
+    subnetId: privSubNetIds[0], // Select an appropriate subnet
+    vpcSecurityGroupIds: [brokerSecGroup.id],
+    // iamInstanceProfile: ecsExecPolicyArn,
+    rootBlockDevice: {
+        volumeSize: 50, // Size in GiB
+        volumeType: 'gp3',
     },
-    deploymentCircuitBreaker: {
-      enable: false,
-      rollback: true,
+    tags: {
+        Name: serviceName,
     },
-    desiredCount: 1,
-    // These min/maxs prevent 2 tasks from being run in parallel
-    // because broker is a singleton but FARGATE does not support DAEMON strategy
-    deploymentMinimumHealthyPercent: 0,
-    deploymentMaximumPercent: 100,
-    enableExecuteCommand: true,
-    taskDefinitionArgs: {
-      family: 'broker',
-      executionRole: {
-        roleArn: executionRole.arn,
-      },
-      taskRole: {
-        roleArn: taskRole.arn,
-      },
-      logGroup: {
-        args: {
-          name: serviceName,
-          retentionInDays: 0,
-          skipDestroy: true,
+  });
+
+  // Create an EBS volume
+  const ebsVolume = new aws.ebs.Volume(serviceName, {
+    availabilityZone: ec2Instance.availabilityZone,
+    size: 100, // Size in GiB
+    type: 'gp3',
+    encrypted: true,
+    tags: {
+        Name: serviceName,
+    },
+  });
+
+  // Attach the EBS volume to the EC2 instance
+  const volumeAttachment = new aws.ec2.VolumeAttachment(serviceName, {
+    instanceId: ec2Instance.id,
+    volumeId: ebsVolume.id,
+    // TODO likely incorrect
+    deviceName: '/dev/sdh',
+  });
+
+  // User data script to configure the instance on launch
+  // TODO completely untested and possibly unsafe. Also doesn't seem to use auth token for ECR?
+  const userData = `#!/bin/bash
+  # Install Docker
+  yum update -y
+  yum install -y docker
+  service docker start
+  usermod -a -G docker ec2-user
+
+  # Retrieve secrets and set up environment variables
+  aws secretsmanager get-secret-value --secret-id ${privateKeySecret.id} --query SecretString --output text > /home/ec2-user/private_key
+  aws secretsmanager get-secret-value --secret-id ${bonsaiSecret.id} --query SecretString --output text > /home/ec2-user/bonsai_api_key
+
+  # Run the Docker container
+  docker run -d --name ${serviceName} \\
+  -e PRIVATE_KEY=$(cat /home/ec2-user/private_key) \\
+  -e BONSAI_API_KEY=$(cat /home/ec2-user/bonsai_api_key) \\
+  -e BONSAI_API_URL=${bonsaiApiUrl} \\
+  -e RPC_URL=${ethRpcUrl} \\
+  -e NO_COLOR=1 \\
+  -e RUST_LOG=broker=debug,boundless_market=debug \\
+  -e RUST_BACKTRACE=1 \\
+  -v /mnt/data:/app/data \\
+  ${dockerTagPath} \\
+  /bin/sh -c "/usr/bin/aws s3 cp s3://boundless-${serviceName}/broker.toml /app/broker.toml && /app/broker --set-verifier-addr ${setVerifierAddr} --boundless-market-addr ${proofMarketAddr} --order-stream-url ${orderStreamUrl} --config-file /app/broker.toml --db-url sqlite:///app/data/broker.db"
+  `;
+
+  // Update the EC2 instance with the user data script
+  const _ec2InstanceWithUserData = new aws.ec2.Instance(serviceName, {
+    ...ec2Instance,
+    userData: userData,
+  }, { dependsOn: [volumeAttachment] });
+
+  // CloudWatch agent configuration
+  const _cloudwatchAgentConfig = new aws.ssm.Parameter(`${serviceName}-cwagent-config`, {
+    name: `/cloudwatch-agent/${serviceName}/config`,
+    type: "String",
+    value: JSON.stringify({
+      logs: {
+        logs_collected: {
+          files: {
+            collect_list: [
+              {
+                file_path: "/var/log/syslog",
+                log_group_name: brokerLogGroup.name,
+                log_stream_name: "{instance_id}/syslog",
+              },
+              {
+                file_path: "/var/log/docker/broker.log",
+                log_group_name: brokerLogGroup.name,
+                log_stream_name: "{instance_id}/broker",
+              },
+            ],
+          },
         },
       },
-      volumes: [
-        {
-          name: 'broker-storage',
-          efsVolumeConfiguration: {
-            fileSystemId: fileSystem.id,
-            rootDirectory: '/',
-          },
-        },
-      ],
-      container: {
-        name: serviceName,
-        image: image.ref,
-        mountPoints: [
-          {
-            sourceVolume: 'broker-storage',
-            containerPath: '/app/data',
-            readOnly: false,
-          },
-        ],
-        entryPoint: ['/bin/sh', '-c'],
-        cpu: 4096,
-        memory: 8192,
-        essential: true,
-        linuxParameters: {
-          initProcessEnabled: true,
-        },
-        command: [
-          `/usr/bin/aws s3 cp s3://$BUCKET/broker.toml /app/broker.toml && /app/broker --set-verifier-address ${setVerifierAddr} --boundless-market-address ${proofMarketAddr} --order-stream-url ${orderStreamUrl} --config-file /app/broker.toml --db-url sqlite:///app/data/broker.db`,
-        ],
-        secrets: [
-          {
-            name: 'PRIVATE_KEY',
-            valueFrom: privateKeySecret.arn,
-          },
-          {
-            name: 'BONSAI_API_KEY',
-            valueFrom: bonsaiSecret.arn,
-          },
-          {
-            name: 'RPC_URL',
-            valueFrom: ethRpcUrlSecret.arn,
-          }
-        ],
-        environment: [
-          { name: 'NO_COLOR', value: '1' },
-          { name: 'RUST_LOG', value: 'broker=debug,boundless_market=debug' },
-          { name: 'RUST_BACKTRACE', value: '1' },
-          { name: 'BONSAI_API_URL', value: bonsaiApiUrl },
-          { name: 'BUCKET', value: brokerS3BucketName }
-        ],
-      },
-    },
+    }),
   });
 
   new aws.cloudwatch.LogMetricFilter(`${serviceName}-error-filter`, {
@@ -348,7 +322,7 @@ export = () => {
       defaultValue: '0',
     },
     pattern: 'ERROR',
-  }, { dependsOn: [service] });
+  }, { dependsOn: [ec2Instance] });
 
   new aws.cloudwatch.LogMetricFilter(`${serviceName}-lock-filter`, {
     name: `${serviceName}-log-lock-filter`,
@@ -360,5 +334,5 @@ export = () => {
       defaultValue: '0',
     },
     pattern: '?"Locked order" ?"locked order" ?"Order locked"',
-  }, { dependsOn: [service] });
+  }), { dependsOn: [ec2Instance] };
 };
