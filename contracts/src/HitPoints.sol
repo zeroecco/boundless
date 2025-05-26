661daa67236ad709b54d352207a785e7375d6384
capossele <angelo@risczero.com>
2025-05-26 14:35:33 +0100

    drop burn

--- contracts/src/HitPoints.sol
+++ contracts/src/HitPoints.sol
@@ -16,7 +16,6 @@ pragma solidity ^0.8.20;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -24,7 +23,7 @@ import "./IHitPoints.sol";
 
 /// @title HitPoints ERC20
 /// @notice Implementation of a restricted transfer token using ERC20
-contract HitPoints is ERC20, ERC20Burnable, ERC20Permit, IHitPoints, AccessControl, Ownable {
+contract HitPoints is ERC20, ERC20Permit, IHitPoints, AccessControl, Ownable {
     // Maximum allowed balance (uint96 max value)
     uint256 constant MAX_BALANCE = type(uint96).max;
     // Role identifier for minting operation
