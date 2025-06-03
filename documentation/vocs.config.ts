d82eef1a1a7b7edd2807fc14bd44caec07ff23f7
Sasha <33594434+sashaaldrick@users.noreply.github.com>
2025-06-03 11:42:42 +0100

    BM-1018: migrating from Bonsai? adoption guide (#712)
    I've tried my best here to follow this proposal document:
    
    
    > Instead of a migration guide, we should direct developers to existing
    Boundless documentation and the Quick Start guide. A concise "adoption
    guide" should outline realistic setup requirements and emphasize the
    long-term advantages of adopting Boundless. This guide would clearly
    indicate which existing resources developers need to consult to
    efficiently use Boundless, without duplicating existing documentation.
    
    ---------
    
    Co-authored-by: pote.eth <81638931+willpote@users.noreply.github.com>

--- documentation/vocs.config.ts
+++ documentation/vocs.config.ts
@@ -65,12 +65,20 @@ const DEVELOPERS_ITEMS = [
         text: "Tutorials",
         items: [
           {
-            text: "Proof Types",
-            link: "/developers/tutorials/proof-types",
+            text: "Callbacks",
+            link: "/developers/tutorials/callbacks",
           },
           {
             text: "Proof Composition",
             link: "/developers/tutorials/proof-composition",
+          },
+          {
+            text: "Proof Types",
+            link: "/developers/tutorials/proof-types",
+          },
+          {
+            text: "Migrating from Bonsai",
+            link: "/developers/tutorials/bonsai",
           },
           {
             text: "Sensitive Inputs",
@@ -79,10 +87,6 @@ const DEVELOPERS_ITEMS = [
           {
             text: "Smart Contract Requestors",
             link: "/developers/tutorials/smart-contract-requestor",
-          },
-          {
-            text: "Callbacks",
-            link: "/developers/tutorials/callbacks",
           },
         ],
       },
@@ -107,12 +111,12 @@ const DEVELOPERS_ITEMS = [
             link: "/developers/smart-contracts/reference",
           },
           {
-            text: "Verifier Contracts",
-            link: "/developers/smart-contracts/verifier-contracts",
-          },
-          {
             text: "Chains & Deployments",
             link: "/developers/smart-contracts/deployments",
+          },
+          {
+            text: "Verifier Contracts",
+            link: "/developers/smart-contracts/verifier-contracts",
           },
         ],
       },
